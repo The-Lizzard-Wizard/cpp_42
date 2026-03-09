392897c15a25424eac11f1a9ea1021ab4f07c840
The-Lizzard-Wizard <gaelchauvet@orange.fr>
2026-03-09 17:12:49 +0100

    int and float cone for the ex01 of cpp 06

--- cpp06/ex00/class/cpp/ScalarConverter.cpp
+++ cpp06/ex00/class/cpp/ScalarConverter.cpp
@@ -3,26 +3,84 @@
 
 ScalarConverter::ScalarConverter() {}
 
-ScalarConverter::ScalarConverter(ScalarConverter &src) {}
+ScalarConverter::ScalarConverter(ScalarConverter &src) { (void)src; }
 
-ScalarConverter &ScalarConverter::operator=(ScalarConverter &src) { return (*this); }
+ScalarConverter &ScalarConverter::operator=(ScalarConverter &src) { (void)src; return (*this); }
 
 ScalarConverter::~ScalarConverter() {}
 
+
+bool isFloatStr(str s)
+{
+	size_t i = 0;
+	bool p = false;
+	if (s.c_str()[0] == '-')
+		i++;
+	if (s.c_str()[0] == '.')
+		return (false);
+	while (s.c_str()[i])
+	{
+		if (s.c_str()[i] == '.')
+		{
+			i++;
+			p = !p;
+			continue ;
+		}
+		if (s.c_str()[i] < 48 || s.c_str()[i] > 57)
+			return (false);
+		i++;
+	}
+	if (p == false)
+		return (false);
+	return (true);
+}
+
+bool isNumStr(str s)
+{
+	size_t i = 0;
+	if (s.c_str()[0] == '-')
+		i++;
+	while (s.c_str()[i])
+	{
+		if (s.c_str()[i] < 48 || s.c_str()[i] > 57)
+			return (false);
+		i++;
+	}
+	return (true);
+}
+
 void PrintChar(str fmt)
 {
+	str res = fmt;
+	std::cout << "char : '" << res << "'" << std::endl;
 }
 
 void PrintInt(str fmt)
 {
+	str res;
+	if (isNumStr(fmt))
+		res = fmt;
+	else
+		res = IMP;
+	std::cout << "int : " << res << std::endl;
 }
 
 void PrintFloat(str fmt)
 {
+	str res = fmt;
+	if (isFloatStr(fmt))
+		res = fmt;
+	else if (isNumStr(fmt))
+		res = fmt;
+	else
+		res = IMP;
+	std::cout << "float : " << res << "f" << std::endl;
 }
 
 void PrintDouble(str fmt)
 {
+	str res = fmt;
+	std::cout << "double : " << res << std::endl;
 }
 
 void ScalarConverter::convert(str fmt)
--- cpp06/ex00/main.cpp
+++ cpp06/ex00/main.cpp
@@ -1,4 +1,8 @@
-int main()
+#include "class/hpp/ScalarConverter.hpp"
+
+int main(int argc, char **argv)
 {
+	if (argc > 1)
+		ScalarConverter::convert(argv[1]);
 	return (0);
 }
