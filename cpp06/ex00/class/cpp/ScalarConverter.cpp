77cc6140eea050da346eddba292182c4b0339311
The-Lizzard-Wizard <gaelchauvet@orange.fr>
2026-03-10 17:24:31 +0100

    float fuuuucking done

--- cpp06/ex00/class/cpp/ScalarConverter.cpp
+++ cpp06/ex00/class/cpp/ScalarConverter.cpp
@@ -12,33 +12,6 @@ ScalarConverter &ScalarConverter::operator=(ScalarConverter &src) { (void)src; r
 
 ScalarConverter::~ScalarConverter() {}
 
-
-bool isFloatStr(str s)
-{
-	size_t i = 0;
-	bool p = false;
-	//check overflow here
-	if (s.c_str()[0] == '-')
-		i++;
-	if (s.c_str()[0] == '.')
-		return (false);
-	while (s.c_str()[i])
-	{
-		if (s.c_str()[i] == '.')
-		{
-			i++;
-			p = !p;
-			continue ;
-		}
-		if (s.c_str()[i] < 48 || s.c_str()[i] > 57)
-			return (false);
-		i++;
-	}
-	if (p == false)
-		return (false);
-	return (true);
-}
-
 bool isNumStr(str s)
 {
 	size_t i = 0;
@@ -53,9 +26,95 @@ bool isNumStr(str s)
 	return (true);
 }
 
+bool isFloatStr(str &s)
+{
+	size_t i = 0;
+	bool p = false;
+	//check overflow here
+	if (s.c_str()[0] == '-')
+		i++;
+	if (s.c_str()[0] == '.')
+		return (false);
+	while (i < s.length() - 1)
+	{
+		if (s.c_str()[i] == '.')
+		{
+			i++;
+			p = !p;
+			continue ;
+		}
+		if (s.c_str()[i] < 48 || s.c_str()[i] > 57)
+			return (false);
+		i++;
+	}
+	if (p == true && s.c_str()[i] == '.')
+		return (false);
+	else if (p == false && s.c_str()[i] == '.')
+	{
+		s += "0";
+		return (true);
+	}
+	if (s.c_str()[i] == 'f')
+	{
+		char isn[2];
+		isn[1] = '\0';
+		isn[0] = s.substr(s.length() - 2, s.length() - 1).c_str()[0];
+		if (p  && !isNumStr(isn))
+			s = s.substr(0, s.length() - 1) + "0";
+		else if (!p)
+			s = s.substr(0, s.length() - 1) + ".0";
+		return (true);
+	}
+	if (p == false)
+		return (false);
+	return (true);
+}
+
+bool isDoubleStr(str s)
+{
+	size_t i = 0;
+	bool p = false;
+	//check overflow here
+	if (s.c_str()[0] == '-')
+		i++;
+	if (s.c_str()[0] == '.')
+		return (false);
+	while (i < s.length() - 1)
+	{
+		if (s.c_str()[i] == '.')
+		{
+			i++;
+			p = !p;
+			continue ;
+		}
+		if (s.c_str()[i] < 48 || s.c_str()[i] > 57)
+			return (false);
+		i++;
+	}
+	if (s.c_str()[i] == 'f' || s.c_str()[i] == '.')
+		return (true);
+	if (p == false)
+		return (false);
+	return (true);
+}
+
 void PrintChar(str fmt)
 {
-	str res = fmt;
+	str res;
+	if (isNumStr(fmt) || isFloatStr(fmt))
+	{
+		char s[2];
+		s[1] = '\0';
+		int c;
+		std::istringstream(fmt) >> c;
+		s[0] = c;
+		if (isprint(c))
+			res = s;
+		else
+			res = NDIP;
+	}
+	else
+		res = IMP;
 	std::cout << "char : '" << res << "'" << std::endl;
 }
 
@@ -83,15 +142,24 @@ void PrintFloat(str fmt)
 	if (isFloatStr(fmt))
 		res = fmt;
 	else if (isNumStr(fmt))
-		res = fmt;
+		res = fmt + ".0";
 	else
 		res = "nan";
-	std::cout << "float : " << res << "f" << std::endl;
+	if (res.c_str()[res.length() - 1] == 'f')
+		std::cout << "float : " << res << std::endl;
+	else
+		std::cout << "float : " << res << "f" << std::endl;
 }
 
 void PrintDouble(str fmt)
 {
 	str res = fmt;
+	if (isDoubleStr(fmt))
+		res = fmt;
+	else if (isNumStr(fmt))
+		res = fmt;
+	else
+		res = "nan";
 	std::cout << "double : " << res << std::endl;
 }
 
