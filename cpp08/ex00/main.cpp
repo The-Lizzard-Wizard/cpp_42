9499058732418f9842ad46253b6cea2e6388d6cd
The-Lizzard-Wizard <gaelchauvet@orange.fr>
2026-05-22 16:28:39 +0200

    add more test

--- cpp08/ex00/main.cpp
+++ cpp08/ex00/main.cpp
@@ -1,9 +1,34 @@
 #include "easyfind.tpp"
+#include <set>
 #include <vector>
 #include <iostream>
 
 int main()
 {
+	std::set<int> set;
+	set.insert(1);
+	set.insert(2);
+	set.insert(5);
+	set.insert(6);
+	set.insert(8);
+
+	try
+	{
+		std::cout << easyfind(set, 2) << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	try
+	{
+		std::cout << easyfind(set, 4) << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+
 	std::vector<int> vec;
 	vec.push_back(1);
 	vec.push_back(2);
